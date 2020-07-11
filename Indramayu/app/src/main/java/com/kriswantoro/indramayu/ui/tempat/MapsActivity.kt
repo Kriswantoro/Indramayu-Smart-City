94fc30ff350f0126342517e48ebfe358decabfe0
nandianugraha <nandiaplaystore@gmail.com>
2020-07-11 14:53:39 +0700

    getIntent from wisata

--- Indramayu/app/src/main/java/com/kriswantoro/indramayu/ui/tempat/MapsActivity.kt
+++ Indramayu/app/src/main/java/com/kriswantoro/indramayu/ui/tempat/MapsActivity.kt
@@ -25,7 +25,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         lat = intent.getDoubleExtra("lat", 0.0)
         lng = intent.getDoubleExtra("lng", 0.0)
-        nama_tempat = intent.getStringExtra("tempat")
+        nama_tempat = intent.getStringExtra("nama")
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
