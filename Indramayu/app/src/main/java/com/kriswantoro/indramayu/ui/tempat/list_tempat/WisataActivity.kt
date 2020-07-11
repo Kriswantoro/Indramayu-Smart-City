75aae6c417fe81862b2d686935055264d6231b51
nandianugraha <nandiaplaystore@gmail.com>
2020-07-11 14:53:20 +0700

    parsing data to maps (masih manual)

--- Indramayu/app/src/main/java/com/kriswantoro/indramayu/ui/tempat/list_tempat/WisataActivity.kt
+++ Indramayu/app/src/main/java/com/kriswantoro/indramayu/ui/tempat/list_tempat/WisataActivity.kt
@@ -1,9 +1,11 @@
 package com.kriswantoro.indramayu.ui.tempat.list_tempat
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,14 +14,17 @@ import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.kriswantoro.indramayu.R
+import com.kriswantoro.indramayu.ui.tempat.MapsActivity
 import com.kriswantoro.indramayu.ui.tempat.list_tempat.wisata.WisataAdapter
 import com.kriswantoro.indramayu.ui.tempat.list_tempat.wisata.WisataModel
+import kotlinx.android.synthetic.main.activity_wisata.*
 import org.json.JSONObject
 import java.lang.Exception
 
 class WisataActivity : AppCompatActivity() {
 
     private val mDataList = ArrayList<WisataModel>()
+    var mDataList2 = ArrayList<WisataModel>()
     private lateinit var adapter: WisataAdapter
 
     @SuppressLint("WrongConstant")
@@ -27,46 +32,59 @@ class WisataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wisata)
 
+        initList()
 
-        val list_wisata = this.findViewById(R.id.list_wisata) as RecyclerView
+        list_wisata.layoutManager = LinearLayoutManager(this)
 
-        list_wisata.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL,  false)
+        adapter = WisataAdapter(mDataList2) {
+            //item adapter udah auto get position
+            val intent = Intent(this, MapsActivity::class.java)
+            intent.putExtra("nama", it.nama_tempat)
+            intent.putExtra("lat", it.lat)
+            intent.putExtra("lng", it.lng)
+            startActivity(intent)
 
+            Toast.makeText(this, it.nama_tempat, Toast.LENGTH_SHORT).show()
+        }
+        list_wisata.adapter = adapter
 
-        val adapter =
-            WisataAdapter(
-                mDataList
-            )
+//        AndroidNetworking.get("http://192.168.100.5:8000/tempatWisata/")
+//            .setPriority(Priority.MEDIUM)
+//            .build()
+//            .getAsJSONObject(object : JSONObjectRequestListener {
+//                override fun onResponse(response: JSONObject?) {
+//                    val jsonArray = response!!.optJSONArray("data")
+//
+//                    try {
+//                        for (i in 0 until jsonArray?.length()!!) {
+//                            val objectWisata = jsonArray?.optJSONObject(i)
+//                            mDataList.add(
+//                                WisataModel(
+//                                    objectWisata.getString("nama_tempat"),
+//                                    objectWisata.getDouble("lat"),
+//                                    objectWisata.getDouble("lng")
+//                                )
+//                            )
+//                        }
+//                    } catch (e: Exception) {
+//
+//                    }
+//
+////                    list_wisata.adapter = adapter
+//                }
+//
+//                override fun onError(anError: ANError?) {
+//                    Toast.makeText(this@WisataActivity, "Kesalahan Jaringan", Toast.LENGTH_LONG)
+//                        .show()
+//                }
+//            })
+    }
 
-        AndroidNetworking.get("http://192.168.100.5:8000/tempatWisata/")
-            .setPriority(Priority.MEDIUM)
-            .build()
-            .getAsJSONObject(object : JSONObjectRequestListener {
-                override fun onResponse(response: JSONObject?) {
-                    val jsonArray = response!!.optJSONArray("data")
-
-                    try {
-                        for (i in 0 until jsonArray?.length()!!){
-                            val objectWisata = jsonArray?.optJSONObject(i)
-                            mDataList.add(
-                                WisataModel(
-                                    objectWisata.getString("nama_tempat"),
-                                    objectWisata.getDouble("lat"),
-                                    objectWisata.getDouble("lng")
-                                )
-                            )
-                        }
-                    } catch (e: Exception){
-
-                    }
-
-                    list_wisata.adapter = adapter
-                }
-
-                override fun onError(anError: ANError?) {
-                    Toast.makeText(this@WisataActivity, "Kesalahan Jaringan", Toast.LENGTH_LONG).show()
-                }
-            })
-
+    fun initList() {
+        mDataList2.add(WisataModel(R.drawable.gambar,"Pantai Glayem", -6.414827, 108.433787))
+        mDataList2.add(WisataModel(R.drawable.friends,"Pantai Tirtamaya", -6.407628, 108.425905))
+        mDataList2.add(WisataModel(R.drawable.i,"Pantai karangsong",-6.305388, 108.368601))
+        mDataList2.add(WisataModel(R.drawable.ibadah,"Pantai Panjiwa", -6.328259, 108.112269))
+        mDataList2.add(WisataModel(R.drawable.w,"Polindra",-6.408163, 108.281704 ))
     }
 }
