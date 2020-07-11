b901d892e37f642bf92144b95994e79287719391
nandianugraha <nandiaplaystore@gmail.com>
2020-07-11 14:52:31 +0700

    adapter wisata

--- Indramayu/app/src/main/java/com/kriswantoro/indramayu/ui/tempat/list_tempat/wisata/WisataAdapter.kt
+++ Indramayu/app/src/main/java/com/kriswantoro/indramayu/ui/tempat/list_tempat/wisata/WisataAdapter.kt
@@ -3,25 +3,28 @@ package com.kriswantoro.indramayu.ui.tempat.list_tempat.wisata
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.kriswantoro.indramayu.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_list_kantor_polisi.view.*
 import kotlinx.android.synthetic.main.item_list_wisata.view.*
 
-class WisataAdapter(val list : ArrayList<WisataModel>) : RecyclerView.Adapter<WisataAdapter.ViewHolder>()  {
+class WisataAdapter(val list: ArrayList<WisataModel>, val listener: (WisataModel) -> Unit) :
+    RecyclerView.Adapter<WisataAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_list_wisata, parent, false)
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_list_wisata, parent, false)
 
-        return ViewHolder(
-            v
-        )
-
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
@@ -30,15 +33,21 @@ class WisataAdapter(val list : ArrayList<WisataModel>) : RecyclerView.Adapter<Wi
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val user: WisataModel = list[position]
-
-        holder.nama_tempat.text = user.nama_tempat
+        holder.bindItem(list[position], listener)
     }
 
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val nama_tempat = itemView.nama_tempat
+        //biasai nama variable lowerUpper
+//        val nama_tempat: TextView = itemView.nama_tempat
+        val namaTempat: TextView = itemView.nama_tempat
+        val fotoWisata: ImageView = itemView.foto_wisata
 
+        fun bindItem(user: WisataModel, listener: (WisataModel) -> Unit) {
+            Picasso.get().load(user.foto_wisata).into(fotoWisata)
+            namaTempat.text = user.nama_tempat
+            itemView.setOnClickListener { listener(user) }
+        }
     }
 }
