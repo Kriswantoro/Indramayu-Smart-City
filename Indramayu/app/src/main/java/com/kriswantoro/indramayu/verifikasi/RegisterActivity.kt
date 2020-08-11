1967ca24ca1c7d0354b5d4ffa97011a78d47ec9e
nandianugraha <nandiaplaystore@gmail.com>
2020-08-12 02:23:50 +0700

    register with image

--- Indramayu/app/src/main/java/com/kriswantoro/indramayu/verifikasi/RegisterActivity.kt
+++ Indramayu/app/src/main/java/com/kriswantoro/indramayu/verifikasi/RegisterActivity.kt
@@ -1,57 +1,49 @@
 package com.kriswantoro.indramayu.verifikasi
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.android.volley.AuthFailureError
 import com.android.volley.toolbox.StringRequest
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.github.dhaval2404.imagepicker.sample.setLocalImage
-import com.kriswantoro.indramayu.MainActivity
 import com.kriswantoro.indramayu.R
-import com.kriswantoro.indramayu.intro.SharedPref
-import com.kriswantoro.indramayu.ui.akun.AkunModel
 import com.kriswantoro.indramayu.util.EndPoint
+import com.kriswantoro.indramayu.util.FileUtil
 import com.kriswantoro.indramayu.util.VolleySingleton
 import com.kriswantoro.indramayu.util.retrofit.model.CheckNikModel
-import com.kriswantoro.indramayu.util.retrofit.network.BaseService
-import com.kriswantoro.indramayu.util.retrofit.network.response.BaseResponse
+import kotlinx.android.synthetic.main.activity_pengaduan.*
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.btn_ganti_foto
 import org.json.JSONException
 import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.io.File
+import java.io.IOException
 
 
 class RegisterActivity : AppCompatActivity() {
-    companion object {
 
-        private const val GITHUB_REPOSITORY = "https://github.com/Dhaval2404/ImagePicker"
-
-        private const val PROFILE_IMAGE_REQ_CODE = 101
-        private const val GALLERY_IMAGE_REQ_CODE = 102
-        private const val CAMERA_IMAGE_REQ_CODE = 103
-    }
-
-    private var mCameraFile: File? = null
-    private var mGalleryFile: File? = null
-    private var mProfileFile: File? = null
+    private var pathImage: String? = ""
+    private val GALLERY = 1
+    private val CAMERA = 2
+    private val PERMISSION_CODE = 1001
 
     private lateinit var foto_profil: ImageView
     private lateinit var edtEmail: EditText
     private lateinit var edtNoTlpn: EditText
 
-    val mNikCheck = ArrayList<CheckNikModel>()
     var nikUser: String = ""
     var namaUser: String = ""
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,35 +60,9 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
-    }
+        btn_ganti_foto.setOnClickListener { showPicture() }
 
-//    fun checkNik(userNik: String){
-//        BaseService.checkNik().checkNik("checkNik", userNik)
-//            .enqueue(object : Callback<BaseResponse<CheckNikModel>>{
-//                override fun onFailure(call: Call<BaseResponse<CheckNikModel>>, t: Throwable) {
-//                    //nothing
-//                }
-//
-//                override fun onResponse(
-//                    call: Call<BaseResponse<CheckNikModel>>,
-//                    response: Response<BaseResponse<CheckNikModel>>
-//                ) {
-//                    val responseData = response.body()?.response
-//                    if (response.body()?.code == 200) {
-//                        mNikCheck.addAll(responseData!!.toList())
-//                        namaUser = responseData[0].namaUser
-//                        nikUser = responseData[0].nikUser
-//                        nama_lengkap.text = namaUser
-//                        btn_daftar.isEnabled = true
-//                    } else if (response.body()?.code == 302){
-//                        nik.text.clear()
-//                        btn_daftar.isEnabled = false
-//                        Toast.makeText(this@RegisterActivity, response.body()?.response.toString(), Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//
-//            })
-//    }
+    }
 
     fun checkNik() {
         val stringRequest = object : StringRequest(
@@ -156,49 +122,79 @@ class RegisterActivity : AppCompatActivity() {
         VolleySingleton.getInstance(this).addToRequestQueue(stringRequest)
     }
 
-    fun pickProfileImage(view: View) {
-        ImagePicker.with(this)
-            // Crop Square image
-            .cropSquare()
-            .setImageProviderInterceptor { imageProvider -> // Intercept ImageProvider
-                Log.d("ImagePicker", "Selected ImageProvider: " + imageProvider.name)
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                val contentURI = data.data
+                try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    pathImage = FileUtil.BitmapToString(bitmap)
+                    Log.i("Success", "Image from gallery is Ready!")
+                    foto_profil.setImageBitmap(bitmap)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Log.e("Error", "Image from gallery is Failure!")
+                }
             }
-            // Image resolution will be less than 512 x 512
-            .maxResultSize(512, 512)
-            .start(PROFILE_IMAGE_REQ_CODE)
+        } else if (requestCode == CAMERA) {
+            if (data != null) {
+                val thum = data.extras
+                try {
+                    val thumbnail = thum?.get("data") as Bitmap
+                    pathImage = FileUtil.BitmapToString(thumbnail)
+                    foto_profil.setImageBitmap(thumbnail)
+                    Log.i("Success", "Image from take picture is Ready!")
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Log.e("Error", "Image from take picture is Failure!")
+                }
+            }
+
+        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            Log.e("TAG", "Path:${ImagePicker.getFilePath(data)}")
-            // File object will not be null for RESULT_OK
-            val file = ImagePicker.getFile(data)!!
-            when (requestCode) {
-                PROFILE_IMAGE_REQ_CODE -> {
-                    mProfileFile = file
-                    foto_profil.setLocalImage(file, true)
-                }
-                GALLERY_IMAGE_REQ_CODE -> {
-                    mGalleryFile = file
-                }
-                CAMERA_IMAGE_REQ_CODE -> {
-                    mCameraFile = file
-                }
+    fun showPicture() {
+        val pictureDialog = AlertDialog.Builder(this)
+        pictureDialog.setTitle("Option to Access")
+        val pic = arrayOf("Capture by Camera")
+        pictureDialog.setItems(pic) { dialog, which ->
+            when (which) {
+                0 -> capturebyCamera()
             }
-        } else if (resultCode == ImagePicker.RESULT_ERROR) {
-            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+        pictureDialog.show()
+    }
+
+    fun choosefromGallery() {
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivityForResult(galleryIntent, GALLERY)
+    }
+
+    fun capturebyCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.let { it1 ->
+                    ContextCompat.checkSelfPermission(
+                        it1,
+                        Manifest.permission.CAMERA
+                    )
+                } ==
+                PackageManager.PERMISSION_DENIED) {
+                val permissions = arrayOf(Manifest.permission.CAMERA);
+                requestPermissions(permissions, PERMISSION_CODE)
+            } else {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA)
+            }
         }
     }
 
     private fun registerUser() {
         val email = edtEmail.text.toString()
         val noTelp = edtNoTlpn.text.toString()
-
-        val fotoProfil =
-            "https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg"
 
         val stringRequest = object : StringRequest(
             Method.POST, EndPoint.URL_REGISTER,
@@ -246,7 +242,7 @@ class RegisterActivity : AppCompatActivity() {
             override fun getParams(): MutableMap<String, String> {
                 val params = HashMap<String, String>()
                 params["nik"] = nikUser
-                params["foto_pengguna"] = fotoProfil
+                params["foto_pengguna"] = ("data:image/jpeg;base64," + pathImage!!)
                 params["nama_pengguna"] = namaUser
                 params["email"] = email
                 params["no_tlpn"] = noTelp
